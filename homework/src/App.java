ba9cb5aced1e78bd38f7c66a5d292ca28960b48b
wangziyan-000 <2302771553@qq.com>
2025-02-24 22:21:35 +0800

    Add files via upload

---
+++ homework/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        SliceoHeaven sliceoheaven = new SliceoHeaven();
+        sliceoheaven.takeOrder("1145","salami",140.00,"salad","coke",120);
+    }
+}
